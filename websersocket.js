ee6fac319e47a915b6820758db393c9f469c39dd
Your Name <you@example.com>
2024-01-10 01:27:48 +0100

    changed websersocket for deno deploy3

--- websersocket.js
+++ websersocket.js
@@ -1,49 +1,46 @@
 import {
     f_websersocket_serve, 
-    f_v_before_return_response__fileserver
+    f_v_before_return_response__fileserver,
+    f_v_before_return_response__proxy
 } from "https://deno.land/x/websersocket@0.2/mod.js"
+
 
 let s_path_file_current = new URL(import.meta.url).pathname;
 let s_path_folder_current = s_path_file_current.split('/').slice(0, -1).join('/'); 
 // console.log(s_path_folder_current)
+// Deno.exit()
+const b_deno_deploy = Deno.env.get("DENO_DEPLOYMENT_ID") !== undefined;
 
 await f_websersocket_serve(
     [
         {
-            b_https: true,
-            n_port: 4343,
+            b_https: (b_deno_deploy) ? false : true,
+            n_port: (b_deno_deploy) ? 80 : (4343),
             s_hostname: 'localhost',
-            s_path_certificate_file: './custom.crt',
-            s_path_key_file: './custom.key',
             f_v_before_return_response: async function(o_request){
-                // important if the connection is secure (https), 
-                // the socket has to be opened with the wss:// protocol
-                // from the client
-                // for client: const socket = new WebSocket(`${window.location.protocol.replace('http', 'ws')}//${window.location.host}`);
-                if(o_request.headers.get('Upgrade') == 'websocket'){
 
-                    const { 
-                        socket: o_socket,
-                        response: o_response
-                    } = Deno.upgradeWebSocket(o_request);
-
-                    o_socket.addEventListener("open", () => {
-                        console.log("a client connected!");
-                        o_socket.send('hello from websocket');
-                    });
-                    
-                    o_socket.addEventListener("message", async (event) => {
-                        console.log(`a message was received ${event}`)
-                    });
-                    
-                    return o_response;
+                let o_url = new URL(o_request.url);
+                if(o_url.pathname == '/'){
+                    return new Response(
+                        await Deno.readTextFile(
+                            `${s_path_folder_current}/localhost/client.html`
+                        ),
+                        { 
+                            headers: {
+                                'Content-type': "text/html"
+                            }
+                        }
+                    );
                 }
+
+
 
                 return f_v_before_return_response__fileserver(
                     o_request,
-                    `${s_path_folder_current}/`
+                    `${s_path_folder_current}/localhost/`
                 )
             }
         },
+
     ]
 )
